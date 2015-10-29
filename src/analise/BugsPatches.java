418b6070c0ea2b63c4a3f901a18490c7c2c5a189
Ana Carla <ana.carlagb@gmail.com>
2015-10-29 12:05:55 -0300

    [test] Test commit #4

--- src/analise/BugsPatches.java
+++ src/analise/BugsPatches.java
@@ -1,15 +1,15 @@
 package analise;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 //Gnome History
 public class BugsPatches {
