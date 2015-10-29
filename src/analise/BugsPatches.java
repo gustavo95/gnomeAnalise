eacdcb31eb5dc344dc80b65732c6ff5057db6a52
Ana Carla <ana.carlagb@gmail.com>
2015-10-29 11:53:38 -0300

    [test] Test commit

--- src/analise/BugsPatches.java
+++ src/analise/BugsPatches.java
@@ -11,7 +11,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-//Gnome
+//Gnome History
 public class BugsPatches {
 	
 	public BugsPatches(){
